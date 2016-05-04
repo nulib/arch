9c35ee6d75aca2d292b0b119c5c31b8454c94bfa
bmquinn <brendan-quinn@northwestern.edu>
2016-05-04 16:28:11 -0500

    Give admins permission to edit ContentBlocks and add link to toolbar

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,11 +6,21 @@ class Ability
 
   self.ability_logic += [:everyone_can_create_curation_concerns]
 
+  def editor_abilities
+    super
+    if current_user.admin?
+      can :create, TinymceAsset
+      can [:create, :update], ContentBlock
+    end
+  end
+
   # Define any customized permissions here.
   def custom_permissions
     if current_user.admin?
       can [:read, :create, :update, :destroy], Role
+      can :manage, :all
     end
+
     # Limits deleting objects to a the admin user
     #
     # if current_user.admin?
