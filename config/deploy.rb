0275b8d8c7eddbb36cc86543a7fa41d6bc6b444b
vagrant <steve@northwestern.edu>
2016-05-18 04:50:50 +0000

    Corrected typo in repo_url Added symlink generation Added config to allow passenger to restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.5.0'
 
 set :application, 'institutional_repository'
-set :repo_url, 'git@github.com/nulib/institutional-repository.git'
+set :repo_url, 'git@github.com:/nulib/institutional-repository.git'
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -40,6 +40,9 @@ set :sidekiq_role, :app
 set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
 set :pty,  false # for Capistrano 3.x
 
+# deprecated way of restarting Passenger but will work for now
+set :passenger_restart_with_touch, true
+
 namespace :deploy do
 
   after :restart, :clear_cache do
@@ -52,3 +55,33 @@ namespace :deploy do
   end
 
 end
+
+  desc "Add symlinks for nufia config files"
+  task :create_symlink do
+    on roles(:app) do
+      execute "ln -s /usr/local/nufia/nufia_config/nufia_config/config/analytics.yml /var/www/nufia/shared/config/analytics.yml"
+            info "Created analytics.yml symlink"
+      execute "ln -s /usr/local/nufia/nufia_config/nufia_config/config/blacklight.yml /var/www/nufia/shared/config/blacklight.yml"
+            info "Created blacklight.yml symlink"
+      execute "ln -s /usr/local/nufia/nufia_config/nufia_config/config/browse_everything_providers.yml /var/www/nufia/shared/config/browse_everything_providers.yml"
+            info "Created browse_everything_providers.yml symlink"
+      execute "ln -s /usr/local/nufia/nufia_config/nufia_config/config/database.yml /var/www/nufia/shared/config/database.yml"
+            info "Created database.yml symlink"
+      execute "ln -s /usr/local/nufia/nufia_config/nufia_config/config/fedora.yml /var/www/nufia/shared/config/fedora.yml"
+            info "Created fedora.yml symlink"
+      execute "ln -s /usr/local/nufia/nufia_config/nufia_config/config/ldap.yml /var/www/nufia/shared/config/ldap.yml"
+            info "Created ldap.yml symlink"
+      execute "ln -s /usr/local/nufia/nufia_config/nufia_config/config/redis.yml /var/www/nufia/shared/config/redis.yml"
+            info "Created redis.yml symlink"
+      execute "ln -s /usr/local/nufia/nufia_config/nufia_config/config/role_map.yml /var/www/nufia/shared/config/role_map.yml"
+            info "Created role_map.yml symlink"
+      execute "ln -s /usr/local/nufia/nufia_config/nufia_config/config/secrets.yml /var/www/nufia/shared/config/secrets.yml"
+            info "Created secrets.yml symlink"
+      execute "ln -s /usr/local/nufia/nufia_config/nufia_config/config/sidekiq.yml /var/www/nufia/shared/config/sidekiq.yml"
+            info "Created sidekiq.yml symlink"
+      execute "ln -s /usr/local/nufia/nufia_config/nufia_config/config/solr.yml /var/www/nufia/shared/config/solr.yml"
+            info "Created solr.yml symlink"
+    end
+  end
+
+after :deploy, ":create_symlink"
