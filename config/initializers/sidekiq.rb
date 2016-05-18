5f5b01053a1c7222b4e31afb499089bef13895f6
vagrant <steve@northwestern.edu>
2016-05-18 04:26:35 +0000

    Added sidekiq initializer to allow it to talk to the correct redis server

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,22 @@
+if Rails.env.staging? && SERVICES.present?
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "redis://nufiarepo-s.library.northwestern.edu:6379/0", namespace: 'sidekiq' }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "redis://nufiarepo-s.library.northwestern.edu:6379/0", namespace: 'sidekiq' }
+  end
+end
+
+
+if Rails.env.production? && SERVICES.present?
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "redis://nufiarepo-plibrary.northwestern.edu:6379/0", namespace: 'sidekiq' }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "redis://nufiarepo-p.library.northwestern.edu:6379/0", namespace: 'sidekiq' }
+  end
+end
