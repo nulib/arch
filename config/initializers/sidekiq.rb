1d40d1611adcad75383e79aadd4761f2d7450f03
Steve DiDomenico <steve@northwestern.edu>
2016-05-24 10:40:09 -0500

    Fixed type in sidekiq initializer for prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -13,7 +13,7 @@ end
 if Rails.env.production?
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: "redis://nufiarepo-plibrary.northwestern.edu:6379/0", namespace: 'sidekiq' }
+    config.redis = { url: "redis://nufiarepo-p.library.northwestern.edu:6379/0", namespace: 'sidekiq' }
   end
 
   Sidekiq.configure_client do |config|
