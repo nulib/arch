afeee517685684ee646b50816a36d10032a6f911
bmquinn <brendan-quinn@northwestern.edu>
2017-05-15 13:45:32 -0500

    Install rspec

--- spec/forms/curation_concerns/generic_work_form_spec.rb
+++
@@ -1,9 +0,0 @@
-# Generated via
-#  `rails generate hyrax:work GenericWork`
-require 'rails_helper'
-
-describe Hyrax::GenericWorkForm do
-  it "has tests" do
-    skip "Add your tests here"
-  end
-end
