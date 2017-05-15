011c4e39d0f8aaa0b3048c6f7f0feab55a0b3f7b
Karen Didrickson <karendid@gmail.com>
2017-05-15 15:02:26 -0500

    Add tests to generic_work_spec.rb

--- spec/models/generic_work_spec.rb
+++ spec/models/generic_work_spec.rb
@@ -3,7 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe GenericWork do
-  it "has tests" do
-    skip "Add your tests here"
-  end
+  it { is_expected.to respond_to(:title) }
+  it { is_expected.to respond_to(:depositor) }
+  it { is_expected.to respond_to(:creator) }
+  it { is_expected.to respond_to(:description) }
 end
