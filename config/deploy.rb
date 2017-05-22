f3f0d25b16da097f965bdb5c4d05bbf483517e17
Karen Didrickson <karendid@gmail.com>
2017-05-22 11:13:07 -0500

    Update capistrano version in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.5.0'
+lock '3.8.1'
 
 set :application, 'institutional_repository'
 set :repo_url, 'git@github.com:/nulib/institutional-repository.git'
