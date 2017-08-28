f86ebb43938e373d4e6319eab84786088363d2ef
Chris Syversen <csyversen@gmail.com>
2017-08-28 15:12:50 -0500

    fixing deprecation messages

--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,6 @@ module Nufia7
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.active_job.queue_adapter = :sidekiq
 
