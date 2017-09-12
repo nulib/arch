9f6a5ad2bf6464ac50b957865504543e98c59d36
Chris Syversen <csyversen@gmail.com>
2017-09-12 14:59:21 -0500

    in progress check in

--- spec/models/solr_document_spec.rb
+++ spec/models/solr_document_spec.rb
@@ -17,19 +17,27 @@ describe SolrDocument do
     let(:date_created) { '2017-05-14' }
     let(:system_created) { '2017-05-14T12:34:56Z' }
     let(:date_modified) { '2017-05-15T12:34:56Z' }
+    let(:doi) { '10.5072/FK2GM88084' }
     let(:document_hash) do
       {
         date_created_tesim: date_created,
         system_modified_dtsi: date_modified,
         system_create_dtsi: system_created,
         language_tesim: ['eng'],
-        title_tesim: ['Work Title']
+        title_tesim: ['Work Title'],
+        doi_tesim: doi
       }
     end
 
     describe '#title' do
       it 'returns the title' do
         expect(subject.title).to eq(['Work Title'])
+      end
+    end
+
+    describe '#doi' do
+      it 'returns the doi' do
+        expect(subject.doi).to eq(['10.5072/FK2GM88084'])
       end
     end
   end
