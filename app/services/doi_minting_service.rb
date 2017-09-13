db9a5b56b2c13c5590d0fb2e3a085aba81bd2d88
Chris Syversen <csyversen@gmail.com>
2017-09-13 12:45:10 -0500

    slight cleanup of service

--- app/services/doi_minting_service.rb
+++ app/services/doi_minting_service.rb
@@ -2,7 +2,7 @@ class DoiMintingService
   include Rails.application.routes.url_helpers
   include ActionDispatch::Routing::PolymorphicRoutes
 
-  attr_reader :work, :identifier
+  attr_reader :work
 
   def self.mint_identifier_for(work)
     DoiMintingService.new(work).run
@@ -10,12 +10,11 @@ class DoiMintingService
 
   def initialize(obj)
     @work = obj
-    @identifier = obj.identifier
   end
 
   def run
     return unless identifier_server_reachable?
-    if identifier.present?
+    if work.doi.present?
       update_metadata
     else
       mint_identifier
@@ -24,68 +23,68 @@ class DoiMintingService
 
   private
 
-    def update_metadata
-      return if minter_user == 'apitest'
-      minter.modify(identifier, metadata)
-    end
+  def mint_identifier
+    work.doi = minter.mint(metadata).id
+    work.save
+  end
 
-    def minter_user
-      Ezid::Client.config.user
-    end
+  def update_metadata
+    return if minter_user == 'apitest'
+    minter.modify(work.doi, metadata)
+  end
 
-    def minter
-      Ezid::Identifier
-    end
+  def minter_user
+    Ezid::Client.config.user
+  end
 
-    # Any error raised during connection is considered false
-    def identifier_server_reachable?
-      Ezid::Client.new.server_status.up?
-    rescue
-      false
-    end
+  def minter
+    Ezid::Identifier
+  end
 
-    def metadata
-      {
-        'datacite.creator' => creator,
-        'datacite.title' => title,
-        'datacite.publisher' => publisher,
-        'datacite.publicationyear' => date_created,
-        'datacite.resourcetype' => resource_type,
-        target: url
-      }
-    end
+  # Any error raised during connection is considered false
+  def identifier_server_reachable?
+    Ezid::Client.new.server_status.up?
+  rescue
+    false
+  end
 
-    def date_created
-      return 'Unknown' if work.date_created.empty?
-      work.date_created.join('; ')
-    end
+  def metadata
+    {
+      'datacite.creator' => creator,
+      'datacite.title' => title,
+      'datacite.publisher' => publisher,
+      'datacite.publicationyear' => date_created,
+      'datacite.resourcetype' => resource_type,
+      target: url
+    }
+  end
 
-    def creator
-      return 'Unknown' if work.creator.empty?
-      work.creator.join('; ')
-    end
+  def creator
+    return 'Unknown' if work.creator.empty?
+    work.creator.join('; ')
+  end
 
-    def publisher
-      return 'Unknown' if work.publisher.empty?
-      work.publisher.join('; ')
-    end
+  def title
+    return 'Unknown' if work.title.empty?
+    work.title.join('; ')
+  end
 
-    def title
-      return 'Unknown' if work.title.empty?
-      work.title.join('; ')
-    end
+  def publisher
+    return 'Unknown' if work.publisher.empty?
+    work.publisher.join('; ')
+  end
 
-    def resource_type
-      return 'Other' if work.resource_type.empty?
-      work.resource_type.join('; ')
-    end
+  def date_created
+    return 'Unknown' if work.date_created.empty?
+    work.date_created.join('; ')
+  end
 
-    def url
-      polymorphic_url(work, host: Rails.application.secrets.host || 'localhost:3333')
-    end
+  def resource_type
+    return 'Other' if work.resource_type.empty?
+    work.resource_type.join('; ')
+  end
 
-    def mint_identifier
-      work.identifier = [minter.mint(metadata).id]
-      work.save
-    end
+  def url
+    polymorphic_url(work, host: Rails.application.secrets.host || 'localhost:3333')
+  end
 end
