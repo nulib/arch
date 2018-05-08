757a46e48833e90f2334c08e861feec29e4f769d
Michael Klein <mbklein@gmail.com>
2018-05-08 16:11:22 -0500

    Refactor configuration to use config gem; add OpenAM SSO

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,29 +16,27 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :ldap_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
   # the account.
   def to_s
-    username
+    email
   end
 
-  before_validation do
-    attrs = self.ldap_entry
-    unless attrs.nil?
-      self.email = attrs[:mail].first
-      self.display_name = attrs[:displayname].first
-      if Rails.env.development?
-        self.display_name = attrs[:displayName].first
-        self.department = attrs[:department].first
-      else
-        self.display_name = attrs[:displayname].first
-        # nuPosition1 is formatted: title$$department$$address$$$mailcode
-        self.department = attrs[:nuPosition1].first.to_s.split("$$").second
+  def self.from_omniauth(auth)
+    username = auth.uid
+    email = auth.info.email
+
+    (User.find_by(username: username) ||
+      User.find_by(email: email) ||
+      User.create(username: username, email: email)).tap do |user|
+        if user.username.nil? || user.email.nil?
+          user.username = username
+          user.email = email
+          user.save
+        end
       end
-    end
   end
 end
