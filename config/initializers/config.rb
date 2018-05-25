47daa95f5eeba0f754a7f3aaeaff2cd92def63b2
Michael Klein <mbklein@gmail.com>
2018-05-25 04:27:29 -0500

    Add zookeeper-related services and improve Dockerfile

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -4,12 +4,7 @@ Config.setup do |config|
 
   # Ability to remove elements of the array set in earlier loaded settings file. For example value: '--'.
   #
-  # config.knockout_prefix = nil
-
-  # Overwrite an existing value when merging a `nil` value.
-  # When set to `false`, the existing value is retained after merge.
-  #
-  # config.merge_nil_values = true
+  config.knockout_prefix = '--'
 
   # Overwrite arrays found in previously loaded settings file. When set to `false`, arrays will be merged.
   #
@@ -17,11 +12,11 @@ Config.setup do |config|
 
   # Load environment variables from the `ENV` object and override any settings defined in files.
   #
-  config.use_env = false
+  config.use_env = true
 
   # Define ENV variable prefix deciding which variables to load into config.
   #
-  # config.env_prefix = 'Settings'
+  config.env_prefix = 'SETTINGS'
 
   # What string to use as level separator for settings loaded from ENV variables. Default value of '.' works well
   # with Heroku, but you might want to change it for example for '__' to easy override settings from command line, where
@@ -37,14 +32,5 @@ Config.setup do |config|
 
   # Parse numeric values as integers instead of strings.
   #
-  # config.env_parse_values = true
-
-  # Validate presence and type of specific config values. Check https://github.com/dry-rb/dry-validation for details.
-  #
-  # config.schema do
-  #   required(:name).filled
-  #   required(:age).maybe(:int?)
-  #   required(:email).filled(format?: EMAIL_REGEX)
-  # end
-
+  config.env_parse_values = true
 end
