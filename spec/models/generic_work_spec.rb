bc2ea227cfffaa359c88ff71f7029ba582addc26
Chris Syversen <csyversen@gmail.com>
2018-06-15 14:01:30 -0500

    getting dois working again and test configs

--- spec/models/generic_work_spec.rb
+++ spec/models/generic_work_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe GenericWork do
   it { is_expected.to respond_to(:creator) }
   it { is_expected.to respond_to(:description) }
   it { is_expected.to respond_to(:doi) }
+
+  context 'doi minting' do
+    let(:work) do
+      described_class.create(title: ['title'])
+    end
+
+    it 'creates a doi after a save' do
+      expect(work.doi).to start_with('doi')
+    end
+  end
 end
