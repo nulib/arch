f0f9213f687ddd2f66c125ce21234f17b31ca1d4
Michael Klein <mbklein@gmail.com>
2018-06-29 11:44:00 -0500

    Configure mailer to use the correct headers

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -3,3 +3,14 @@ if Settings&.email&.mailer.present?
   require 'aws-sdk-rails' if mailer == :aws_sdk
   ActionMailer::Base.delivery_method = mailer
 end
+
+Hyrax::ContactForm.class_eval do
+  def headers
+    {
+      subject: "#{Hyrax.config.subject_prefix} #{subject}",
+      to: Hyrax.config.contact_email,
+      from: Hyrax.config.contact_email,
+      reply_to: email
+    }
+  end
+end
