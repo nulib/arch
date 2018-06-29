89ae7c2ac965ffe68c4a430c94b0ea235040920f
Michael Klein <mbklein@gmail.com>
2018-06-29 11:10:16 -0500

    Convert mailer setting to a symbol

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,4 +1,5 @@
 if Settings&.email&.mailer.present?
-  require 'aws-sdk-rails' if Settings.email.mailer.to_sym == :aws_sdk
-  ActionMailer::Base.delivery_method = Settings.email.mailer
+  mailer = Settings.email.mailer.to_sym
+  require 'aws-sdk-rails' if mailer == :aws_sdk
+  ActionMailer::Base.delivery_method = mailer
 end
