82b61b43b360d8eea85a884cdfe80aacb8bc4b8a
Brendan Quinn <brendan-quinn@northwestern.edu>
2020-06-04 10:49:00 -0500

    Make GenericWork a Hyrax::Work

--- app/models/generic_work.rb
+++ app/models/generic_work.rb
@@ -1,26 +1,14 @@
-# Generated via
-#  `rails generate hyrax:work GenericWork`
-class GenericWork < ActiveFedora::Base
-  include ::Hyrax::WorkBehavior
-  # Change this to restrict which works can be added as a child.
-  # self.valid_child_concerns = []
+class GenericWork < Hyrax::Work
+  include Hyrax::Schema(:basic_metadata)
 
-  validates :title, presence: { message: 'Your work must have a title.' }
+  attribute :doi, Valkyrie::Types::String
 
-  self.human_readable_type = 'Work'
-  self.indexer = GenericWorkIndexer
+  # TODO: Replace these hooks with changesets?
+  # after_save do
+  #   DoiMintingService.mint_identifier_for(self) if doi.nil?
+  # end
 
-  property :doi, predicate: ::RDF::Vocab::DataCite.doi, multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  after_save do
-    DoiMintingService.mint_identifier_for(self) if doi.nil?
-  end
-
-  before_destroy do
-    DoiMintingService.tombstone_identifier_for(self) if doi.present?
-  end
-
-  include ::Hyrax::BasicMetadata
+  # before_destroy do
+  #   DoiMintingService.tombstone_identifier_for(self) if doi.present?
+  # end
 end
